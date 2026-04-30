599016887b97266b46a323a66b2c15ad6bf36479
Marc <jmarc296@gmail.com>
2026-04-30 13:03:27 -0400

    demo: wire full demo flow

---
+++ components/demo/DistrictDemoShell.tsx
@@ -0,0 +1,17 @@
+﻿import DemoExecutiveSummary from "./DemoExecutiveSummary"
+import DemoEscalationMoment from "./DemoEscalationMoment"
+import DemoOwnershipShift from "./DemoOwnershipShift"
+import DemoActionTable from "./DemoActionTable"
+
+export default function DistrictDemoShell() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <div className="mx-auto max-w-6xl space-y-6">
+        <DemoExecutiveSummary />
+        <DemoEscalationMoment />
+        <DemoOwnershipShift />
+        <DemoActionTable />
+      </div>
+    </div>
+  )
+}
