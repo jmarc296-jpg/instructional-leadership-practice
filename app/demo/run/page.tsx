991f1b9f48a62df074bb07bc92d793d7d6969360
Marc <jmarc296@gmail.com>
2026-05-01 09:54:32 -0400

    Wire real executive summary into demo (dynamic output)

--- app/demo/run/page.tsx
+++ app/demo/run/page.tsx
@@ -1,10 +1,12 @@
-﻿export default function DemoRunPage() {
+﻿import { buildExecutiveSummary } from '../utils/executiveSummary'
+export default function DemoRunPage() {
   return (
     <main className="min-h-screen p-10">
       <h1>LeadSharper Demo Run</h1>
     </main>
   );
 }
+
 
 
 
